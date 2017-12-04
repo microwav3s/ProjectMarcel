3785bfc3c1dd9042bc83a1df6384679b407c8262
microwav3s <microwav3s@googlemail.com>
2017-12-04 21:10:06 +0100

    Add test content for automated build checks

--- src/IR_P01.java
+++ src/IR_P01.java
@@ -6,15 +6,6 @@ import org.apache.lucene.store.RAMDirectory;
 public class IR_P01 {
 
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MySearchGUI window = new MySearchGUI();
-					window.mainFrame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		System.out.println("IR_P01 launched.");
 	}
 }
